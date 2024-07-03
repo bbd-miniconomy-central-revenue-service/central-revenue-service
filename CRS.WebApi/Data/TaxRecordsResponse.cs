3237acc8c2bcfa9fa69d5c4b7d58166291473500
anza <anza@bbd.co.za>
2024-07-03 20:05:44 +0200

    records history endpoint

---
+++ CRS.WebApi/Data/TaxRecordsResponse.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.Json.Serialization;
+
+namespace CRS.WebApi.Data;
+
+public class TaxRecordsResponse
+{
+    [JsonPropertyName("id")]
+    public Guid Id { get; set; }
+
+    [JsonPropertyName("type")]
+    public string? Type { get; set; }
+
+    [JsonPropertyName("hasPaid")]
+    public int HasPaid { get; set; }
+
+    [JsonPropertyName("amountOwing")]
+    public decimal AmountOwing { get; set; }
+
+    [JsonPropertyName("taxType")]
+    public string? TaxType { get; set; }
+
+}
